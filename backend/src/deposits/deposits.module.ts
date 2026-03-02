59c1d359fb671d4944797dc32131f378fd40dbfd
gleepgorp <staphmaster2002@gmail.com>
2026-03-03 00:51:44 +0800

    [Fix] Add AuthModule and UsersModule imports to DepositsModule

--- backend/src/deposits/deposits.module.ts
+++ backend/src/deposits/deposits.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from '../db/db.module';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 import { DepositsController } from './deposits.controller';
 import { DepositsService } from './deposits.service';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AuthModule, UsersModule],
   controllers: [DepositsController],
   providers: [DepositsService],
 })
